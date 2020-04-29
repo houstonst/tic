f0014865c22a117086140d02d249a329c841a287
houstonst <matthew.houston@westpoint.edu>
2020-04-29 00:23:29 -0500

    changes turn-based system

--- server.py
+++ server.py
@@ -7,8 +7,7 @@ HOST = "127.0.0.1"
 PORT = 2000
 
 #specify server parameters
-users = []
-is_turn = {0: True, 1: False}
+users = {} #stored as {conn: boolean} to indicate whether it's their turn
 
 #establish socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -16,7 +15,7 @@ server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.bind((HOST, PORT))
 
 #define this as a listening socket
-server_socket.listen(2)
+server_socket.listen()
 
 #close server socket if users have all left the session
 def close_session():
@@ -27,12 +26,14 @@ def close_session():
 def remove_user(conn):
 	if conn in users:
 		conn.close()
-		users.remove(conn)
+		users.pop(conn)
 		if (len(users) == 0):
 			close_session()
 
 #user functionality
-def handle_user(conn, addr, num):
+def handle_user(conn, addr):
+	global users
+
 	#receive username
 	username = conn.recv(1024)
 	readable_username = username.decode()
@@ -46,29 +47,28 @@ def handle_user(conn, addr, num):
 			remove_user(conn)
 			break
 		else:
-			if is_turn[num] == False:
+			if users[conn] == False:
 				print("Wait until the other user takes their turn...")
 			else:
 				print("[{}]: {}".format(readable_username, readable_data))
-			if num == 0:
-				is_turn[num] = False
-				is_turn[num+1] = True
-			else:
-				is_turn[num] = False
-				is_turn[num-1] = True
-			#after they print something, is_turn = false
-			#other users is_turn = true
-
+				users = {key: True for key in users}
+				users[conn] = False
 
 #run the server
 while True:
 	try:
-		#accept new users
+		#accept new user connection
 		conn, addr = server_socket.accept()
-		users += [conn]
-		num = users.index(conn)
-		#start a thread for the new client
-		_thread.start_new_thread(handle_user, (conn, addr, num))
 
+		#populate dictionary
+		if True in users.values():
+			users[conn] = False
+		else:
+			users[conn] = True
+
+		#start a thread for the new client
+		_thread.start_new_thread(handle_user, (conn, addr))
+
+	#no client connected. Quietly halt server.
 	except:
 		break
